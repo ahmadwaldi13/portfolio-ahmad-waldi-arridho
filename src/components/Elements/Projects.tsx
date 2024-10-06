bef5fb5a90e97fb527c57d41ba6e343bacf00a0e
Ahmad Waldi Arridho <ahmad.waldi@digits.id>
2024-10-06 20:06:49 +0700

    update: project2

--- src/components/Elements/Projects.tsx
+++ src/components/Elements/Projects.tsx
@@ -59,6 +59,60 @@ const Projects = () => {
             />
           </div>
         </li>
+
+        <li className="mb-12">
+          <div className="group relative grid gap-4 pb-1 sm:grid-cols-8 sm:gap-8">
+            <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden rounded-md transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-800/50 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg" />
+            <div className="z-10 sm:order-2 sm:col-span-6">
+              <h3>
+                <a
+                  href="https://kembangsetaman13.vercel.app/"
+                  className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300  group/link text-base"
+                  target="_blank"
+                >
+                  <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                  <span>Product Showcase App</span>
+                </a>
+              </h3>
+              <p className="mt-2 text-sm leading-normal text-slate-400">
+                The Product Showcase App is a specialized e-commerce platform
+                designed to display a curated selection of products and redirect
+                users to external e-commerce sites for purchases. The app is
+                built with a dual-interface system featuring a guest and admin
+                panel.
+              </p>
+              <ul className="mt-2 flex flex-wrap">
+                <li className="mr-1.5 mt-2">
+                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">
+                    NextJS
+                  </div>
+                </li>
+                <li className="mr-1.5 mt-2">
+                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">
+                    TypeScript
+                  </div>
+                </li>
+                <li className="mr-1.5 mt-2">
+                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">
+                    Laravel
+                  </div>
+                </li>
+                <li className="mr-1.5 mt-2">
+                  <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-5 text-teal-300">
+                    Mysql
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <img
+              alt="Build a eproc"
+              src="projects/project2.png"
+              width="200"
+              height="48"
+              className="rounded border-2 border-slate-200/10 transition group-hover:border-slate-200/30 mt-2 sm:order-1 sm:col-span-2 sm:translate-y-1"
+            />
+          </div>
+        </li>
       </ul>
     </section>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,3 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import LandingPage from '@/components/layouts/LandingPage'
 
 export default function Home() {
